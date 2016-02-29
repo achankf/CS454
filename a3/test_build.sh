93e258f474a290d2efbaa03a2a45cc8178e52ce2
Alfred Chan <alfred8844@gmail.com>
2016-02-29 13:16:00 -0500

    renaming librpc to src; updated makefile

--- a3/test_build.sh
+++ a3/test_build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -xe
 
+SRC=src
 TEST=PartialTestCode
+DFLAG=-ggdb
+WFLAG="-pedantic -Wextra -Wall"
 
-make -C librpc
-cp librpc/librpc.a $TEST
-make -C $TEST
+make -C $SRC DFLAG="$DFLAG" WFLAG="$WFLAG"
+cp $SRC/librpc.a $TEST
+make -C $TEST DFLAG="$DFLAG" WFLAG="$WFLAG"
