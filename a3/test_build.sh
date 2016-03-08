6a35c143e4705b5924fedb2442f7ecf0f04c5a26
Alfred Chan <alfred8844@gmail.com>
2016-03-08 12:58:42 -0500

    updated makefiles

--- a3/test_build.sh
+++ a3/test_build.sh
@@ -6,6 +6,6 @@ DFLAG=-ggdb
 WFLAG="-pedantic -Wextra -Wall"
 
 make -C $SRC DFLAG="$DFLAG" WFLAG="$WFLAG"
-cp src/librpc.a src/binder test
+cp src/librpc.a test
 cp src/binder .
 make -C $TEST DFLAG="$DFLAG" WFLAG="$WFLAG"
